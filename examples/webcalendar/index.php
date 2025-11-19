5ac95a5b26837907a52eb0deba5915d994bd768e
John R. D'Orazio <priest@johnromanodorazio.com>
2025-11-20 00:31:43 +0100

    remove ununsed imports

--- examples/webcalendar/index.php
+++ examples/webcalendar/index.php
@@ -3,7 +3,6 @@
 require '../../vendor/autoload.php';
 
 use LiturgicalCalendar\Components\ApiClient;
-use LiturgicalCalendar\Components\CalendarRequest;
 use LiturgicalCalendar\Components\ApiOptions;
 use LiturgicalCalendar\Components\ApiOptions\Input;
 use LiturgicalCalendar\Components\ApiOptions\PathType;
